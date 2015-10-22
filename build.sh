40210474ac1e402d5cd3a6d51728344f6685c61a
t-era <t-era@ray.ocn.ne.jp>
2015-10-22 20:26:53 +0900

    ビルドスクリプト改良(?)

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 export SRCDIR=src/
 export DESTDIR=dest/
 
-#rdmd -unittest -main ${SRCDIR}tests.d
-dmd -release -od$DESTDIR -of${DESTDIR}numlin $SRCDIR*.d ${SRCDIR}input/*.d
-
+if test "$1" = "test"
+then
+  rdmd -unittest -main ${SRCDIR}tests.d
+elif test "$1" = "run"
+then
+  shift
+  rdmd ${SRCDIR}main.d $@
+else
+ dmd -release -od$DESTDIR -of${DESTDIR}numlin $SRCDIR*.d ${SRCDIR}input/*.d
+fi
